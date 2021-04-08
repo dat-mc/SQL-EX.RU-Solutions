018480a1b0df845257a2af9cdb8e775a32002151
Mikhail Bokov <mixail.bokov9@gmail.com>
2021-04-08 12:32:22 +0700

    Add files via upload

---
+++ DML/Exercise: 3.sql
@@ -0,0 +1,5 @@
+/* Add the model 4444 with code=22, speed=1200 and price=1350 into the PC table.
+
+The absent features should be supplied with the default values for corresponding columns.*/
+
+INSERT INTO PC (code, model, speed, price) VALUES(22, 4444, 1200, 1350)
