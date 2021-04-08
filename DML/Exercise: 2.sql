e8cad8bf58be37ead581789c03ff8bec67a80a92
Mikhail Bokov <mixail.bokov9@gmail.com>
2021-04-08 12:26:41 +0700

    Add files via upload

---
+++ DML/Exercise: 2.sql
@@ -0,0 +1,8 @@
+/* Add to the Product table following products from maker Z:
+printer model 4003, PC model 4001 and laptop model 4002*/
+
+INSERT INTO Product (maker, model, type)
+VALUES
+    ('Z', 4003, 'printer'),
+    ('Z', 4001, 'PC'),
+    ('Z', 4002, 'laptop');
